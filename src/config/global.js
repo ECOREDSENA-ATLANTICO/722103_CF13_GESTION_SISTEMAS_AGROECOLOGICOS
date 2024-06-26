8766d3fcffe71279a9f6daec5d66d515b45592e8
AndresDiazPinto <andresvis565@gmail.com>
2024-06-26 14:35:18 -0500

    Empaquetado
    Se empaqueta componente

--- src/config/global.js
+++ src/config/global.js
@@ -99,8 +99,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download:
-          'downloads/722103_Gestión de sistemas agroecológicos_CF13.pdf',
+        download: 'downloads/722103_CF13_DU.pdf',
       },
       {
         icono: 'fas fa-download',
